1f52d5ac0cc06e9d16ed53796b720826b9482734
IndumathiPatalay <indu.patalay@gmail.com>
2026-07-18 00:08:48 +0530

    Frontend added

---
+++ Agents/app.py
@@ -0,0 +1,45 @@
+from turtle import pd
+
+from Agents import main_app
+import streamlit as st
+from Agents.main_app import process_app 
+
+# --- Define your backend function ---
+def process_input(question : str) -> dict:
+    response = process_app(question)
+    return response
+
+# --- Initialize session state ---
+if "history" not in st.session_state:
+    st.session_state.history = []
+if "question" not in st.session_state:
+    st.session_state.question = ""
+
+
+# --- Build the Streamlit UI ---
+st.title("Intelligent Model Selector Demo")
+
+# Input fields
+Question = st.text_input("Enter your Question:")
+#task = st.selectbox("Choose a task:", ["Summarization", "Translation", "Question Answering"])
+
+
+
+# Submit button
+if st.button("Submit"):
+    # Call the backend function with inputs
+    result = process_input(Question)
+    st.session_state.question = ""
+
+    # Display the result
+    st.success("Result from backend function:")
+    #st.write(result)
+    st.write(f"question: {Question}")
+    st.write(f"Final Answer: {result['final_answer']}")
+    st.write(f"Model Name: {result['modelname']}")
+    st.write(f"Prompt Tokens: {result['Prompt_tokens']}")
+    st.write(f"Completion Tokens: {result['Completion_tokens']}")
+    st.write(f"Total Tokens: {result['Total_tokens']}")
+    
+    
+    
